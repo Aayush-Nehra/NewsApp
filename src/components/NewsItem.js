d6caa1d235716582c844f2a2e72b528e5494daa4
AN095380 <Aayush.Nehra@cerner.com>
2023-04-04 23:40:53 +0530

    dynamically creating div for news element

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description,imageUrl,url} = this.props
     return (
       <div>
-        This is my news item inside news component
+        <div className="card" style={{width: '18rem'}}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={url} target='_blank' className="btn btn-primary">Know More</a>
+          </div>
+        </div>
       </div>
     )
   }
