f01e4506da42a8653cd11ce84b3af529b0b8fdab
AN095380 <Aayush.Nehra@cerner.com>
2023-04-19 00:10:54 +0530

    refactoring code for better understanding

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -6,13 +6,13 @@ export class NewsComponent extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false,
+      componentLoading: false,
       pageNumber: 1
     }
   }
 
   async componentDidMount() {
-    let newsArticles = await this.getNewsArticles();
+    let newsArticles = await this.getNewsArticles(this.state.pageNumber);
     this.setState({ articles: newsArticles })
   }
 
@@ -25,20 +25,22 @@ export class NewsComponent extends Component {
 
   gotoNextPage = async ()=>{
     console.log("nextPage")
-    let nextPageNumber = this.state.pageNumber + 1;
-    let newsArticles = await this.getNewsArticles(nextPageNumber)
+    let currentPageNumber = this.state.pageNumber;
+    let nextPageNumber = currentPageNumber + 1;
+    let nextPageNewsArticles = await this.getNewsArticles(nextPageNumber)
     this.setState({
       pageNumber: nextPageNumber,
-      articles: newsArticles
+      articles: nextPageNewsArticles
     })
   }
 
   gotoPreviousPage = async ()=>{
-    let prevPageNumber = this.state.pageNumber - 1;
-    let newsArticles = await this.getNewsArticles(prevPageNumber)
+    let currentPageNumber = this.state.pageNumber;
+    let prevPageNumber = currentPageNumber - 1;
+    let prevPageNewsArticles = await this.getNewsArticles(prevPageNumber)
     this.setState({
       pageNumber: prevPageNumber,
-      articles: newsArticles
+      articles: prevPageNewsArticles
     })
   }
 
@@ -57,7 +59,7 @@ export class NewsComponent extends Component {
             })}
           </div>
           <div className="container d-flex justify-content-between">
-          <button type="button" disabled={this.state.pageNumber<=1}onClick={this.gotoPreviousPage} className="btn btn-dark">&#8592; Previous</button>
+          <button type="button" disabled={this.state.pageNumber<=1} onClick={this.gotoPreviousPage} className="btn btn-dark">&#8592; Previous</button>
           <button type="button" onClick={this.gotoNextPage} className="btn btn-dark">Next &#8594;</button>
         </div>
         </div>
