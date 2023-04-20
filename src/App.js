fa010f0a051f7b65fb55947b442d86d9f874a4d8
AN095380 <Aayush.Nehra@cerner.com>
2023-04-20 22:55:39 +0530

    adding routing for news categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import NewsComponent from './components/NewsComponent';
@@ -9,10 +9,28 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <NewsComponent/>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<NewsComponent key="default" category="general" />} />
+            <Route path="business" element={<NewsComponent key="business" category="business" />} />
+            <Route path="entertainment" element={<NewsComponent key="entertainment" category="entertainment" />} />
+            <Route path="general" element={<NewsComponent key="general" category="general" />} />
+            <Route path="health" element={<NewsComponent key="health" category="health" />} />
+            <Route path="science" element={<NewsComponent key="science" category="science" />} />
+            <Route path="sports" element={<NewsComponent key="sports" category="sports" />} />
+            <Route path="technology" element={<NewsComponent key="technology" category="technology" />} />
+          </Routes>
+        </BrowserRouter>
+
       </div>
     )
   }
 }
-
+// business
+// entertainment
+// general
+// health
+// science
+// sports
+// technology
