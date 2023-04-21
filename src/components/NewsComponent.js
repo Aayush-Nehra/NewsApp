5e55c8faab2b2b31bf8df84db45eaeb4e0828cdf
AN095380 <Aayush.Nehra@cerner.com>
2023-04-21 18:19:39 +0530

    making app more attractive

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -3,27 +3,40 @@ import NewsItem from './NewsItem'
 import Loader from './Loader';
 import configData from '../main/resources/config.json'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class NewsComponent extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       totalArticles: 0,
       isArticleLoading: false,
       pageNumber: 1
     }
+    document.title = `News - ${this.toTitleCase(this.props.category)}`
   }
+
+  toTitleCase = (phrase) => {
+    return phrase
+      .toLowerCase()
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  };
 
   async componentDidMount() {
-    this.loadArtilcles();
-    await this.displayLoadedArticles(this.state.pageNumber);
+    //this.loadArtilcles();
+    await this.displayLoadedArticlesBySettingState(this.state.pageNumber);
   }
 
-  async displayLoadedArticles(pageNumber) {
+  async displayLoadedArticlesBySettingState(pageNumber) {
     let newsArticlesObject = await this.getNewsArticlesObject(pageNumber);
+    debugger;
+    let allNewsArticles = this.state.articles.concat(newsArticlesObject.articles)
     this.setState({
-      articles: newsArticlesObject.articles,
+      // articles: newsArticlesObject.articles,
+      articles: allNewsArticles,
       totalArticles: newsArticlesObject.totalResults,
       isArticleLoading: false,
       pageNumber: pageNumber
@@ -39,21 +52,28 @@ export class NewsComponent extends Component {
   async getNewsArticlesObject(pageNumber) {
     // let newsApiUrl = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4ee17a8d3034488383d8fd86c4b89dcf&${this.props.pageSize}&page=${pageNumber}`;
     let newsApiUrl = this.getNewsApiRequestUrl(pageNumber);
-    let rawNewsArticles = await fetch(newsApiUrl);
-    let parsedNewsArticles = await rawNewsArticles.json();
-    return parsedNewsArticles;
+    let newsArticlesObject = await fetch(newsApiUrl);
+    let parsedNewsArticlesObject = await newsArticlesObject.json();
+    return parsedNewsArticlesObject;
   }
 
   gotoNextPage = async () => {
     this.loadArtilcles()
     let nextPageNumber = this.getNextPageNumber();
-    this.displayLoadedArticles(nextPageNumber);
+    this.displayLoadedArticlesBySettingState(nextPageNumber);
   }
 
-  gotoPreviousPage = async () => {
-    let prevPageNumber = this.getPreviousPageNumber();
-    this.displayLoadedArticles(prevPageNumber);
+  loadMoreArticles = async () => {
+    //this.loadArtilcles()
+    let nextPageNumber = this.getNextPageNumber();
+    this.displayLoadedArticlesBySettingState(nextPageNumber);
   }
+
+  // gotoPreviousPage = async () => {
+  //   this.loadArtilcles()
+  //   let prevPageNumber = this.getPreviousPageNumber();
+  //   this.displayLoadedArticlesBySettingState(prevPageNumber);
+  // }
 
   getNewsApiRequestUrl(pageNumber) {
     return `${configData.NEWSAPI_URL}?country=in&category=${this.props.category}&apiKey=${configData.NEWSAPI_KEY}&pageSize=${configData.NEWS_APPLICATION_PAGE_SIZE}&page=${pageNumber}`;
@@ -75,21 +95,30 @@ export class NewsComponent extends Component {
     return (
       <div>
         <div className="container text-center my-3">
-          <h1 tabIndex="0">Top News Headlines</h1>
+          <h1>Top News Headlines - {this.toTitleCase(this.props.category)}</h1>
+          <InfiniteScroll
+              dataLength={this.state.totalArticles}
+              next={this.loadMoreArticles}
+              hasMore={true}
+              loader={<h4>Loading...</h4>}
+            >
           <div className="row">
-            <div className="text-center">
+            {/* <div className="text-center">
               {this.state.isArticleLoading && <Loader />}
-            </div>
-            {!this.state.isArticleLoading && this.state.articles.map((article) => {
-              return <div className="col-md-4 my-3" key={article.url}>
-                <NewsItem title={article.title ? article.title : ""} imageUrl={article.urlToImage} description={article.description ? article.description : ""} url={article.url} author={article.author} date={article.publishedAt} />
-              </div>
-            })}
+            </div> */}
+            
+              {this.state.articles.map((article) => {
+                return <div className="col-md-4 my-3" key={article.url}>
+                  <NewsItem title={article.title ? article.title : ""} imageUrl={article.urlToImage} description={article.description ? article.description : ""} url={article.url} author={article.author} date={article.publishedAt} articleSource={article.source.name} />
+                </div>
+              })}
+            
           </div>
-          <div className="container d-flex justify-content-between">
+          </InfiniteScroll>
+          {/* <div className="container d-flex justify-content-between">
             <button type="button" disabled={this.state.pageNumber <= 1} onClick={this.gotoPreviousPage} className="btn btn-dark">&#8592; Previous</button>
             <button type="button" disabled={this.state.pageNumber >= (Math.ceil(this.state.totalArticles / 15))} onClick={this.gotoNextPage} className="btn btn-dark">Next &#8594;</button>
-          </div>
+          </div> */}
         </div>
 
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description,imageUrl,url,author,date} = this.props
+    let { title, description, imageUrl, url, author, date, articleSource } = this.props
     return (
       <div>
         <div className="card">
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
+            <span className="position-absolute top-0 start-1 translate-middle badge rounded-pill bg-danger">
+              {articleSource}
+            </span>
             <h5 className="card-title text-truncate">{title}</h5>
             <p className="card-text">{description}</p>
-            <p class="card-text"><small class="text-muted">Written By: {author?author:"Author Unknown"}</small></p>
-            <p class="card-text"><small class="text-muted">Published On: {new Date(date).toDateString()}</small></p>
+            <p className="card-text"><small className="text-muted">Written By: {author ? author : "Author Unknown"}</small></p>
+            <p className="card-text"><small className="text-muted">Published On: {new Date(date).toDateString()}</small></p>
             <a href={url} target='_blank' className="btn btn-primary">Know More</a>
           </div>
         </div>
