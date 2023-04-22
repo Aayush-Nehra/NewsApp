b5cc4043c03d8ec5beac96b2d3480b23de074e50
AN095380 <Aayush.Nehra@cerner.com>
2023-04-22 13:10:15 +0530

    updating routes

--- src/App.js
+++ src/App.js
@@ -12,10 +12,9 @@ export default class App extends Component {
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path="/" element={<NewsComponent key="default" category="general" />} />
+            <Route path="/" element={<NewsComponent key="general" category="general" />} />
             <Route path="business" element={<NewsComponent key="business" category="business" />} />
             <Route path="entertainment" element={<NewsComponent key="entertainment" category="entertainment" />} />
-            <Route path="general" element={<NewsComponent key="general" category="general" />} />
             <Route path="health" element={<NewsComponent key="health" category="health" />} />
             <Route path="science" element={<NewsComponent key="science" category="science" />} />
             <Route path="sports" element={<NewsComponent key="sports" category="sports" />} />
